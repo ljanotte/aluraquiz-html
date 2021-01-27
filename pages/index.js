2b1805b216cc62f5517e341f272825285fa8a9b0
Leandro José Janotte <ljanotte@gmail.com>
2021-01-27 00:23:09 -0300

    Update imersão react V2

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router'
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 // const BackgroundImage = styled.div `
 //   background-image: url(${db.bg});
@@ -12,7 +14,7 @@ import GitHubCorner from '../src/components/GitHubCorner'
 //   background-position: center;
 // `;
 
-export const QuizContainer = styled.div `
+export const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
   padding-top: 45px;
@@ -23,18 +25,40 @@ export const QuizContainer = styled.div `
   }
 `;
 
-
-
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+  console.log('retorno do useState', name);
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>AluniQuiz - Modelo Html</title>
+      </Head>
       <QuizContainer>
         <Widget>
           <Widget.Header>
             <h1> Titulo</h1>
           </Widget.Header>
           <Widget.Content>
-            <p> Paragrafo ... </p>
+            <form onSubmit={ function(infoDoEvento) {
+              infoDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submisão por meio do react')
+            }}
+            > 
+              <input
+                onChange={function(infosDoEvento) {
+                  console.log(infosDoEvento.target.value);
+                  //State
+                  // name = infosDoEvento.target.value;
+                  setName(infosDoEvento.target.value)
+                }}
+                placeholder="Digite seu nome" />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
@@ -43,9 +67,9 @@ export default function Home() {
             <p> Paragrafo ... </p>
           </Widget.Content>
         </Widget>
-        <Footer/>
+        <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/ljanotte"/>
+      <GitHubCorner projectUrl="https://github.com/ljanotte" />
     </QuizBackground>
-  )
+  );
 }
